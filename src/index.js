acc5849af3b80204c586ac1241607094d3b1c0cf
TheSeagullDev <67653706+TheSeagullDev@users.noreply.github.com>
2025-04-25 20:46:33 -0400

    Make it

--- src/index.js
+++ src/index.js
@@ -1,1 +1,23 @@
 import "./styles.css";
+
+function initializeDropdown(dropdownElement) {
+  const dropdownItems = dropdownElement.children[1].children;
+  for (const item of dropdownItems) {
+    item.style.display = "none";
+  }
+  dropdownElement.addEventListener("mouseover", () => {
+    for (const item of dropdownItems) {
+      item.style.display = "block";
+    }
+  });
+  dropdownElement.addEventListener("mouseout", () => {
+    for (const item of dropdownItems) {
+      item.style.display = "none";
+    }
+  });
+}
+
+initializeDropdown(document.querySelector("#dropdown1"));
+initializeDropdown(document.querySelector("#dropdown2"));
+
+export default initializeDropdown;
